b646b993b9bb5928db62c4e7bab0a6155920b357
NthabyMogoboya <56308431+NthabyMogoboya@users.noreply.github.com>
2020-03-06 23:13:02 +0200

    style

---
+++ crime.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crime Acts Solutions</title>
+</head>
+<body>
+    <div class="murder"></div>
+    <div class="rape"></div>
+    <div class="hijack"></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
+</body>
+</html>
