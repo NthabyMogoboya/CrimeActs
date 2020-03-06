34e2e11a71c002f4e73fdfbf8742b730f7ff93d4
Moora <43159712+MooraBlack@users.noreply.github.com>
2020-03-07 01:26:48 +0200

    Update dracs

--- victim_dashboard.php
+++ victim_dashboard.php
@@ -107,7 +107,7 @@
 
       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
+        <h1 class="h2">We are Strategic Legal Advisors</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">
             <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -116,8 +116,16 @@
         </div>
       </div>
 
-      <canvas class="my-4 w-100 chartjs-render-monitor" id="myChart" width="547" height="230" style="display: block; width: 547px; height: 230px;"></canvas>
-      
+      <div class="infor1">
+          <p>
+          South African entrepreneurs seem to be in agreement that businesses in South Africa are faced with a maze of red tape cramping their business style, and making it very hard to safely negotiate all the potential transgressions they may unwittingly be committing: BEE compliance, Corporate compliance, submitting annual returns, statutory due diligence reviews, knowing about and understanding all legislation applicable to the company – to mention but a few.
+
+          Even if the entrepreneur has a sound knowledge of all the requirements and restrictions, the key question remains whether there will be ample time available to take care of the core business and run a profitable business after addressing all the repetitive red tape?
+
+          Would it not make a world of positive difference if all the non-core but very important aspects of the business could be taken care of – professionally – leaving the entrepreneur to do what he or she is primarily in business for: to run a successful business?
+          </p>
+        </div>
+
       <h2>Section title</h2>
       
 
