968c8301924e4ffb2153f4e22be15243a21ceb8b
senseitumelo <tmotjopi@gmail.com>
2020-03-07 01:38:02 +0200

    ...

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
      <img src="images/default.png" width="150" height="60" alt=""  href="#">
     </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-       <span class="navbar-toggler-icon"></span>
+  
+     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
