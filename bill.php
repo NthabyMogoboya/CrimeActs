533a7361fc5559dab82aa665210e9e80df26fcf2
senseitumelo <tmotjopi@gmail.com>
2020-03-07 01:24:57 +0200

    navigation bar

--- bill.php
+++ bill.php
@@ -52,38 +52,5 @@
     </ul>
   </div>
 </nav>
-<!--corousel-->
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    
-      <img src="images/justice.jpeg" width="1950" height="440" class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h1>The Bill Of Rights</h1>
-        <p>Nobody is above the law</p>
-        <a href="bill.php" class="btn btn-primary">View Bill of Rights</a>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="images/default.png" width="1950" height="440" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="images/default.png" width="1950" height="440" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-  </body>
+</body>
 </html>
