ba123a6a10cb784489a84c8d7df89d2ba547a8bc
ashoktut <ashoktut.2@gmail.com>
2020-03-07 09:47:42 +0200

    admin

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$databaseHost = 'localhost';
+$databaseName = 'justicedata';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
